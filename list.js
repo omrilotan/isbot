c7f2b5026677ebaac9717fd2d9f309114f233a34
James Sullivan <james.sullivan@massdrop.com>
2018-11-20 12:45:46 -0800

    Typo

--- list.js
+++ list.js
@@ -763,7 +763,7 @@ module.exports = [
     'photon/',
     'picosearch/',
     'pingspot/',
-    'p:interestbot',
+    'pinterestbot',
     'pockey-gethtml/',
     'pockey-gethtml/x\\.xx',
     'pockey/x\\.x\\.x',
