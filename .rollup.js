739039bb2c0d7a8038b34963693734e6f4c4ca3a
Omri Lotan <516342+omrilotan@users.noreply.github.com>
2019-11-21 18:59:46 +0200

    Add browser testing (#56)

---
+++ .rollup.js
@@ -0,0 +1,14 @@
+const commonjs = require('rollup-plugin-commonjs');
+const json = require('@rollup/plugin-json');
+
+module.exports = {
+  input: 'tests/browser.js',
+  output: {
+    file: 'tests/.browser-tests.js',
+    format: 'iife'
+  },
+  plugins: [
+    commonjs(),
+    json()
+  ]
+}
