4e072bb2b6386a4c26d2ca2f45f3f3471dc09fdb
Omri <516342+omrilotan@users.noreply.github.com>
2020-05-19 07:44:37 +0000

    better ignores

--- scripts/download-fixtures.sh
+++
@@ -1,5 +0,0 @@
-echo "Download crawler list from user-agents.net"
-curl -f -d 'browser_type=bot-crawler&download=txt' https://user-agents.net/download > tests/fixtures/user-agents.net.bot-crawler.txt
-
-echo "Download crawler list from crawler-user-agents repo"
-curl -f https://raw.githubusercontent.com/monperrus/crawler-user-agents/master/crawler-user-agents.json > tests/fixtures/crawler-user-agents-monperrus.json
